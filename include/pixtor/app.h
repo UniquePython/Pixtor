db7ef3890a3c62758e1c2969295a954e95b7af99
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-09-20 10:32:55 +0530

    code: Get a window on screen

---
+++ include/pixtor/app.h
@@ -0,0 +1,12 @@
+#ifndef APP_H_
+#define APP_H_
+
+typedef struct
+{
+    unsigned int width;
+    unsigned int height;
+    const char *title;
+
+} App;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <raylib.h>
 
+#include "pixtor/app.h"
+
 int main(void)
 {
+    App app = {
+        .width = 900,
+        .height = 600,
+        .title = "Pixtor",
+    };
+
+    InitWindow(app.width, app.height, app.title);
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
